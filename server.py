056e5864f920303278542f0b916e3f32fca75bf1
pluja <github_pluja@r3d.red>
2020-09-18 16:49:26 +0200

    New endpoints

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from yt_data import channel as ytchannel
 from yt_data import search as ytsearch
 from yt_data import video as ytvideo
+from tw_data import user as twuser
 from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from json import dumps
@@ -22,24 +23,39 @@ class Channel(Resource):
         channel = ytchannel.get_channel_info(channelId, includeVideos=includeVideos, page=page)
         return channel # Fetches first column that is Employee ID
 
-api.add_resource(Channel, '/api/channel/<string:channelId>', 
-                          '/api/channel/<string:channelId>/page/<int:page>',
-                          '/api/channel/<string:channelId>/info/<int:includeVideos>') # 0 = False
+api.add_resource(Channel, '/api/yt/channel/<string:channelId>', 
+                          '/api/yt/channel/<string:channelId>/page/<int:page>',
+                          '/api/yt/channel/<string:channelId>/info/<int:includeVideos>') # 0 = False
 
 class Video(Resource):
     def get(self, videoId):
         video = ytvideo.get_video_info(videoId)
         return video
-api.add_resource(Video, '/api/video/<string:videoId>')
+api.add_resource(Video, '/api/yt/video/<string:videoId>')
 
 class Search(Resource):
     def get(self, search_terms, page=1, autocorrect=1, sort=0, filters={"time":0, "type":0, "duration":0}):
         search = ytsearch.search_by_terms(search_terms, page, autocorrect, sort, filters)
         return search
-api.add_resource(Search, '/api/search/<string:search_terms>', 
-                         '/api/search/<string:search_terms>/page/<int:page>',
-                         '/api/search/<string:search_terms>/sort/<int:sort>',
-                         '/api/search/<string:search_terms>/page/<int:page>/sort/<int:sort>')
+api.add_resource(Search, '/api/yt/search/<string:search_terms>', 
+                         '/api/yt/search/<string:search_terms>/page/<int:page>',
+                         '/api/yt/search/<string:search_terms>/sort/<int:sort>',
+                         '/api/yt/search/<string:search_terms>/page/<int:page>/sort/<int:sort>')
+
+parser = reqparse.RequestParser()
+parser.add_argument('str_list', type=str, action='append')
+class twitterUserInfo(Resource):
+    def get(self, username, page=1):
+        feed = twuser.get_uer_info(username)
+        return feed                      
+api.add_resource(twitterUserInfo, '/api/tw/user/<string:username>')
+
+class twitterUserTweets(Resource):
+    def get(self, username, page=1):
+        tweets = twuser.get_tweets(username, page)
+        return tweets
+api.add_resource(twitterUserTweets, '/api/tw/user/<string:username>/tweets', 
+                                    '/api/tw/user/<string:username>/tweets/page/<int:page>')
 
 if __name__ == '__main__':
      app.run()
--- tw_data/user.py
+++ tw_data/user.py
@@ -1,3 +1,4 @@
+from flask import Markup
 from requests_futures.sessions import FuturesSession
 from werkzeug.datastructures import Headers
 from concurrent.futures import as_completed
@@ -11,62 +12,105 @@ import urllib
 import json
 import re
 
-def getFeed(usernames):
-    '''
-    Returns feed tweets given a set of usernames
-    '''
-    feedTweets = []
-    with FuturesSession() as session:
-        futures = [session.get('{instance}{user}'.format(instance=NITTERINSTANCE, user=u)) for u in usernames]
-        for future in as_completed(futures):
-            res = future.result().content.decode('utf-8')
-            html = BeautifulSoup(res, "html.parser")
-            userFeed = html.find_all('div', attrs={'class':'timeline-item'})
-            if userFeed != []:
-                    for post in userFeed[:-1]:
-                        tweet = {}
-                        date_time_str = post.find('span', attrs={'class':'tweet-date'}).find('a')['title'].replace(",","")
-                        time = datetime.datetime.now() - datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
-                        if time.days >=7:
-                            continue
+##########################
+#### Config variables ####
+##########################
+NITTERINSTANCE = 'https://nitter.net/'
 
-                        if post.find('div', attrs={'class':'pinned'}):
-                            if post.find('div', attrs={'class':'pinned'}).find('span', attrs={'icon-pin'}):
-                                continue
-                        
-                        tweet['originalPoster'] = post.find('a', attrs={'class':'username'}).text
-                        tweet['twitterName'] = post.find('a', attrs={'class':'fullname'}).text
-                        tweet['timeStamp'] = datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
-                        tweet['date'] = post.find('span', attrs={'class':'tweet-date'}).find('a').text
-                        tweet['content'] = Markup(post.find('div',  attrs={'class':'tweet-content'}))
-                        
-                        if post.find('div', attrs={'class':'retweet-header'}):
-                            tweet['username'] = post.find('div', attrs={'class':'retweet-header'}).find('div', attrs={'class':'icon-container'}).text
-                            tweet['isRT'] = True
-                        else:
-                            tweet['username'] = newPost.op
-                            tweet['isRT'] = False
-                        
-                        tweet['profilePic'] = NITTERINSTANCE+post.find('a', attrs={'class':'tweet-avatar'}).find('img')['src'][1:]
-                        url = NITTERINSTANCE + post.find('a', attrs={'class':'tweet-link'})['href'][1:]
-                        if post.find('div', attrs={'class':'quote'}):
-                            tweet['isReply'] = True
-                            tweet['quote'] = post.find('div', attrs={'class':'quote'})
-                            if tweet['quote'].find('div',  attrs={'class':'quote-text'}):
-                                tweet['replyingTweetContent'] = Markup(tweet['quote'].find('div',  attrs={'class':'quote-text'}))
-                                
-                            if tweet['quote'].find('a', attrs={'class':'still-image'}):
-                                tweet['replyAttachedImg'] = NITTERINSTANCE+tweet['quote'].find('a', attrs={'class':'still-image'})['href'][1:]
-                            
-                            if tweet['quote'].find('div', attrs={'class':'unavailable-quote'}):
-                                tweet['replyingUser']="Unavailable"
-                            else:
-                                tweet['replyingUser']=tweet['quote'].find('a',  attrs={'class':'username'}).text
-                            post.find('div', attrs={'class':'quote'}).decompose()
+def get_uer_info(username):
+    response = urllib.request.urlopen('{instance}{user}'.format(instance=NITTERINSTANCE, user=username)).read()
+    #rssFeed = feedparser.parse(response.content)
 
-                        if post.find('div',  attrs={'class':'attachments'}):
-                            if not post.find(class_='quote'):
-                                if  post.find('div',  attrs={'class':'attachments'}).find('a', attrs={'class':'still-image'}):
-                                    attachedImg = NITTERINSTANCE + post.find('div',  attrs={'class':'attachments'}).find('a')['href'][1:]
-                        feedTweets.append(tweet)
-    return feedTweets
+    html = BeautifulSoup(str(response), "lxml")
+    if html.body.find('div', attrs={'class':'error-panel'}):
+        return False
+    else:
+        html = html.body.find('div', attrs={'class':'profile-card'})
+
+        if html.find('a', attrs={'class':'profile-card-fullname'}):
+            fullName = html.find('a', attrs={'class':'profile-card-fullname'}).getText().encode('latin1').decode('unicode_escape').encode('latin1').decode('utf8')
+        else:
+            fullName = None
+        
+        if html.find('div', attrs={'class':'profile-bio'}):
+            profileBio = html.find('div', attrs={'class':'profile-bio'}).getText().encode('latin1').decode('unicode_escape').encode('latin1').decode('utf8')
+        else:
+            profileBio = None
+
+        user = {
+            "profileFullName":fullName,
+            "profileUsername":html.find('a', attrs={'class':'profile-card-username'}).string.encode('latin_1').decode('unicode_escape').encode('latin_1').decode('utf8'),
+            "profileBio":profileBio,
+            "tweets":html.find_all('span', attrs={'class':'profile-stat-num'})[0].string,
+            "following":html.find_all('span', attrs={'class':'profile-stat-num'})[1].string,
+            "followers":numerize.numerize(int(html.find_all('span', attrs={'class':'profile-stat-num'})[2].string.replace(",",""))),
+            "likes":html.find_all('span', attrs={'class':'profile-stat-num'})[3].string,
+            "profilePic":"{instance}{pic}".format(instance=NITTERINSTANCE, pic=html.find('a', attrs={'class':'profile-card-avatar'})['href'][1:])
+        }
+        return user
+
+def get_tweets(user, page=1):        
+    feed = urllib.request.urlopen('{instance}{user}'.format(instance=NITTERINSTANCE, user=user)).read()
+    #Gather feedPosts
+    res = feed.decode('utf-8')
+    html = BeautifulSoup(res, "html.parser")
+    feedPosts = get_feed_tweets(html)
+
+    if page == 2:
+        nextPage = html.find('div', attrs={'class':'show-more'}).find('a')['href']
+        print('{instance}{user}{page}'.format(instance=NITTERINSTANCE, user=user, page=nextPage))
+        feed = urllib.request.urlopen('{instance}{user}{page}'.format(instance=NITTERINSTANCE, user=user, page=nextPage)).read()
+        res = feed.decode('utf-8')
+        html = BeautifulSoup(res, "html.parser")
+        feedPosts = get_feed_tweets(html)
+    return feedPosts
+
+def get_feed_tweets(html):
+    feedPosts = []
+    userFeed = html.find_all('div', attrs={'class':'timeline-item'})
+    if userFeed != []:
+        for post in userFeed[:-1]:
+            if 'show-more' in str(post):
+                continue
+            date_time_str = post.find('span', attrs={'class':'tweet-date'}).find('a')['title'].replace(",","")
+
+            if post.find('div', attrs={'class':'pinned'}):
+                if post.find('div', attrs={'class':'pinned'}).find('span', attrs={'icon-pin'}):
+                    continue
+
+            tweet = {}
+            tweet['op'] = post.find('a', attrs={'class':'username'}).text
+            tweet['twitterName'] = post.find('a', attrs={'class':'fullname'}).text
+            tweet['timeStamp'] = str(datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S'))
+            tweet['date'] = post.find('span', attrs={'class':'tweet-date'}).find('a').text
+            tweet['content'] = Markup(post.find('div',  attrs={'class':'tweet-content'}).decode_contents())
+            
+            if post.find('div', attrs={'class':'retweet-header'}):
+                tweet['username'] = post.find('div', attrs={'class':'retweet-header'}).find('div', attrs={'class':'icon-container'}).text
+                tweet['isRT'] = True
+            else:
+                tweet['username'] = tweet['op']
+                tweet['isRT'] = False
+            
+            tweet['profilePic'] = NITTERINSTANCE+post.find('a', attrs={'class':'tweet-avatar'}).find('img')['src'][1:]
+            tweet['url'] = NITTERINSTANCE + post.find('a', attrs={'class':'tweet-link'})['href'][1:]
+            if post.find('div', attrs={'class':'quote'}):
+                tweet['isReply'] = True
+                quote = post.find('div', attrs={'class':'quote'})
+                if quote.find('div',  attrs={'class':'quote-text'}):
+                    tweet['replyingTweetContent'] = Markup(quote.find('div',  attrs={'class':'quote-text'}))
+                    
+                if quote.find('a', attrs={'class':'still-image'}):
+                    tweet['replyAttachedImg'] = NITTERINSTANCE+quote.find('a', attrs={'class':'still-image'})['href'][1:]
+                
+                tweet['replyingUser']=quote.find('a',  attrs={'class':'username'}).text
+                post.find('div', attrs={'class':'quote'}).decompose()
+
+            if post.find('div',  attrs={'class':'attachments'}):
+                if not post.find(class_='quote'):
+                    if  post.find('div',  attrs={'class':'attachments'}).find('a', attrs={'class':'still-image'}):
+                        tweet['attachedImg'] = NITTERINSTANCE + post.find('div',  attrs={'class':'attachments'}).find('a')['href'][1:]
+            feedPosts.append(tweet)
+    else:
+        return {"emptyFeed": True}
+    return feedPosts
